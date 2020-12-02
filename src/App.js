a730a597a5d851b57eb94b8cff5a32968bca5ded
olabiranj <olabiranj@gmail.com>
2020-12-02 10:57:28 +0100

    certificate page implemented

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
-import Stepper from './pages/Stepper'
+import Stepper from './pages/Stepper';
+import Certificate from './pages/Certificate';
 import Feeder from "./components/Project-profile/Feeder";
 import Inspection from "./components/Project-profile/Inspection";
 import MaterialsEquipment from "./components/Material-methods/Materials_Equipmet" 
@@ -13,6 +14,7 @@ function App() {
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/stepper" exact component={Stepper} />
+          <Route path="/certificate" exact component={Certificate} />
           <Route path="/feeder" exact component={Feeder}/>
           <Route path="/feeder/inspection" exact component={Inspection}/>
           <Route path="/materials" exact component={MaterialsEquipment}/>
